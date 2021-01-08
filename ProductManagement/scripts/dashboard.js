0130de66d384fda7ad8245c9ceff79508ace3b78
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2021-01-08 20:35:59 +0700

    update

--- ProductManagement/scripts/dashboard.js
+++ ProductManagement/scripts/dashboard.js
@@ -1,4 +1,10 @@
-var products = ["Sony Xperia", "IP 6s", "IP 7s", "IP 10"];
+// var products = ["Sony Xperia", "IP 6s", "IP 7s", "IP 10"];
+var products = [];
+if (window.localStorage.getItem("products") != null) {
+    products = JSON.parse(window.localStorage.getItem("products"));
+} else {
+    window.localStorage.setItem("products", JSON.stringify(["Sony Xperia", "IP 6s", "IP 7s", "IP 10"]));
+}
 
 function showProduct() {
     let tbProduct = document.getElementById("tbProduct");
@@ -11,15 +17,12 @@ function showProduct() {
                                     </td>
                                     <td>
                                         <a href="javascript:;" class="btn btn-success" onclick='editProduct(${i})'>Edit</a>
+                                        <a href="javascript:;" class="btn btn-success" onclick='getProduct(${i})'>Modify</a>
                                         <a href="" class="btn btn-danger">Remove</a>
                                     </td>
                                 </tr>`
     })
     document.getElementById("productCount").innerHTML = `${products.length} products`;
-}
-
-function reset() {
-    document.getElementById("productName").value = "";
 }
 
 function isNullOrEmpty(str) {
@@ -59,14 +62,30 @@ function addProduct() {
         alert("Product name is required.");
     } else {
         productName = format(productName);
-        if (isExistProduct(productName) != -1) {
-            alert(`Product name ${productName} is exist`);
-        } else {
-            products.push(productName);
-            showProduct();
+        let productId = parseInt(document.getElementById("productId").value);
+        //add new product
+        if (productId == -1) {
+            if (isExistProduct(productName) != -1) {
+                alert(`Product name ${productName} is exist`);
+            } else {
+                products.push(productName);
+                window.localStorage.setItem("products", JSON.stringify(products));
+                reset();
+            }
         }
+        //update product by id
+        else {
+            let pos = isExistProduct(productName)
+            if (pos != -1 && pos != productId) {
+                alert(`Product name ${productName} is exist`);
+            } else {
+                products[productId] = productName;
+                window.localStorage.setItem("products", JSON.stringify(products));
+                reset();
+            }
+        }
+        showProduct();
     }
-    reset();
 }
 
 function editProduct(id) {
@@ -83,6 +102,25 @@ function editProduct(id) {
             showProduct();
         }
     }
+}
+
+function getProduct(id) {
+    let productName = products[id];
+    let addEditForm = document.getElementsByClassName("add-product")[0];
+    addEditForm.children[0].value = productName;
+    addEditForm.children[1].text = "Update";
+    addEditForm.children[2].classList.remove('d-none');
+    document.getElementById("productId").value = id;
+    document.getElementById("form-title").innerHTML = "Update Product";
+}
+
+function reset() {
+    let addEditForm = document.getElementsByClassName("add-product")[0];
+    addEditForm.children[0].value = "";
+    addEditForm.children[1].text = "Add";
+    addEditForm.children[2].classList.add('d-none');
+    document.getElementById("productId").value = -1;
+    document.getElementById("form-title").innerHTML = "Add New Product";
 }
 
 function documentReady() {
