0ed4f317b4cd12fe230773f48c5088826cdfe4a7
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2021-01-10 15:22:14 +0700

    update

--- ProductManagement/scripts/dashboard.js
+++ ProductManagement/scripts/dashboard.js
@@ -11,14 +11,16 @@ function showProduct() {
     tbProduct.innerHTML = "";
     products.forEach(function(v, i) {
         tbProduct.innerHTML += `
-                                <tr>
+                                <tr id="tr_${i}">
                                     <td>
                                         ${v}
                                     </td>
                                     <td>
+                                        <a href="javascript:;" class="btn btn-warning" onclick='inlineEditProduct(${i})'>Inline Edit</a>
+                                        <a href="javascript:;" class="btn btn-danger d-none" onclick='inlineEdit(${i})'>Inline Update</a>
                                         <a href="javascript:;" class="btn btn-success" onclick='editProduct(${i})'>Edit</a>
                                         <a href="javascript:;" class="btn btn-success" onclick='getProduct(${i})'>Modify</a>
-                                        <a href="" class="btn btn-danger">Remove</a>
+                                        <a href="javascript:;" class="btn btn-danger" onclick='remove(${i})'>Remove</a>
                                     </td>
                                 </tr>`
     })
@@ -121,6 +123,87 @@ function reset() {
     addEditForm.children[2].classList.add('d-none');
     document.getElementById("productId").value = -1;
     document.getElementById("form-title").innerHTML = "Add New Product";
+}
+
+function inlineEditProduct(id) {
+    let tr = document.getElementById(`tr_${id}`);
+    let data = tr.children[0].innerText;
+    tr.children[0].innerHTML = `<input type="text" id="pn_${id}" value="${data}"/> <a href='javascript:;' onclick="inlineReset(${id})"><i style="color:red" class="fa fa-times"></i></a>`;
+    tr.children[1].children[0].classList.add('d-none');
+    tr.children[1].children[1].classList.remove('d-none');
+    let pnInupt = document.getElementById(`pn_${id}`);
+    pnInupt.focus();
+    pnInupt.setSelectionRange(pnInupt.value.length, pnInupt.value.length);
+}
+
+function inlineReset(id) {
+    let tr = document.getElementById(`tr_${id}`);
+    let oldData = products[id];
+    tr.children[0].innerHTML = oldData;
+    tr.children[1].children[0].classList.remove('d-none');
+    tr.children[1].children[1].classList.add('d-none');
+}
+
+function inlineEdit(id) {
+    let productName = document.getElementById(`pn_${id}`).value;
+    let pos = isExistProduct(productName)
+    if (pos != -1 && pos != productId) {
+        alert(`Product name ${productName} is exist`);
+    } else {
+        products[id] = productName;
+        window.localStorage.setItem("products", JSON.stringify(products));
+        inlineReset(id)
+    }
+}
+
+function remove(id) {
+    if (id < 0 || id >= products.length) {
+        // alert(`Can not found id = ${id}`);
+        showMessage(`Can not found id = ${id}`, 0);
+    } else {
+        let confirmed = window.confirm('Are you sure remove this product?');
+        if (confirmed) {
+            products.splice(id, 1);
+            window.localStorage.setItem("products", JSON.stringify(products));
+            showMessage(`Product has been removed success.`, 1);
+            showProduct();
+        }
+    }
+}
+
+function showMessage(msg, status) {
+    let msgArea = document.getElementById('message');
+    let message = msgArea.children[0];
+    message.innerText = msg;
+    // msgArea.classList.remove('d-none');
+    msgArea.className = "";
+    msgArea.classList.add('row');
+    switch (status) {
+        //error
+        case 0:
+            {
+                msgArea.classList.add('error');
+                break;
+            }
+            //success
+        case 1:
+            {
+                msgArea.classList.add('success');
+                break;
+            }
+    }
+    autoCloseMessage();
+}
+
+function closeMessage() {
+    let msgArea = document.getElementById('message');
+    msgArea.classList.add('d-none');
+}
+
+function autoCloseMessage() {
+    setTimeout(() => {
+        closeMessage();
+    }, 10000);
 }
 
 function documentReady() {
